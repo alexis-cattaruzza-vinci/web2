3e7e8270a8e285680c71d22f01b747d074120b1d
Alexis <alexis.cattaruzza@student.vinci.be>
2023-10-02 14:02:13 +0200

    1.8 : API : linter & formatter

--- exercices/1.8/app.js
+++ exercices/1.8/app.js
@@ -1,13 +1,11 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var filmsRouter = require('./routes/films');
+const filmsRouter = require('./routes/films');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -29,11 +27,8 @@ app.use((req, res, next) => {
   next();
 });
 
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/films', filmsRouter);
 
 module.exports = app;
